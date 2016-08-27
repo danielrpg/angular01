16766fa75e98dc7be737cb51410cc5797a8dabd0
danielrpg <daniel.fernandezgp@gmail.com>
2016-08-27 10:19:56 -0400

    Conexion a base de datos PostgreSQL

---
+++ conexionBD/conexionbd/src/main/java/com/ejemplo02/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.ejemplo02.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public String index(){
+		return "Hola desde Spring";
+	}
+	
+}
